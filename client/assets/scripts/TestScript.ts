34938b15738046414abc7b4b25e89fa83546f7b2
Vykri <114884270+Vykri@users.noreply.github.com>
2024-11-25 12:14:55 -0600

    Initial commit

---
+++ client/assets/scripts/TestScript.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('TestScript')
+export class TestScript extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
